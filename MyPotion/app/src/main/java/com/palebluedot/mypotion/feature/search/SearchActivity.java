42797c6b884835e73e2b5edcaa4b660afbde8e3c
HaYeong <hy78733@gmail.com>
2021-08-04 03:36:08 +0900

    Create | Update: layout resources, Adapter, attatch LiveData

--- MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchActivity.java
+++ MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchActivity.java
@@ -1,16 +1,73 @@
 package com.palebluedot.mypotion.feature.search;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.palebluedot.mypotion.R;
+import com.palebluedot.mypotion.data.model.PotionItem;
+import com.palebluedot.mypotion.data.model.SearchResults;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchViewModel model;
+
+    private EditText mSearchView;
+    ListView mListView;
+
+    private ImageView mSearchButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
+    private CardView mPagination;
+    TextView mPageNo;
+    TextView mMaxpageNo;
+    int pageNo = 1;
+    int maxPageNo = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mPagination = findViewById(R.id.search_pagination);
+        mPageNo = findViewById(R.id.curr_pageNo);
+        mMaxpageNo = findViewById(R.id.max_pageNo);
+        mSearchView = findViewById(R.id.search_view);
+        mListView = findViewById(R.id.result_list_view);
+        mSearchButton = findViewById(R.id.search_button);
+        mPreviousButton = findViewById(R.id.previous_button);
+        mNextButton = findViewById(R.id.next_button);
+
+        final SearchListAdapter adapter = new SearchListAdapter(new ArrayList<PotionItem>());
+        mListView.setAdapter(adapter);
+
+        model = new ViewModelProvider(this).get(SearchViewModel.class);
+        //observe : model의 LiveData를 관찰한다.
+        model.get().observe(this, new Observer<SearchResults>() {
+            @Override
+            public void onChanged(SearchResults searchResults) {
+                adapter.setData(searchResults.getResults());
+                pageNo = searchResults.getPageNo();
+                maxPageNo = searchResults.getMaxPageNo();
+                mPageNo.setText(pageNo);
+                mMaxpageNo.setText(maxPageNo);
+            }
+        });
+
+        mSearchButton.setOnClickListener(v -> {
+            String keyword = mSearchView.getText().toString();
+            model.search(keyword, 1);
+        });
     }
 }
--- MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchViewModel.java
+++ MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchViewModel.java
@@ -4,10 +4,25 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.palebluedot.mypotion.data.model.SearchResults;
+import com.palebluedot.mypotion.data.repository.results.SearchResultsRepository;
 
 public class SearchViewModel extends AndroidViewModel {
-
+    private SearchResultsRepository repository;
+    private LiveData<SearchResults> mData;
     public SearchViewModel(@NonNull Application application) {
         super(application);
+        repository = SearchResultsRepository.getInstance(application);
+        mData = repository.getData();
+    }
+
+    public LiveData<SearchResults> search(String keyword, int pageNo) {
+        return repository.getSearchResults(keyword, pageNo);
+    }
+
+    public LiveData<SearchResults> get() {
+        return mData;
     }
 }
