d835249e5dd32b0edfef91f35dfa0c744e9addfb
HaYeong <hy78733@gmail.com>
2021-08-04 00:40:02 +0900

    Create: Activity, ViewModel, layout

---
+++ MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.palebluedot.mypotion.feature.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.palebluedot.mypotion.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
---
+++ MyPotion/app/src/main/java/com/palebluedot/mypotion/feature/search/SearchViewModel.java
@@ -0,0 +1,13 @@
+package com.palebluedot.mypotion.feature.search;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class SearchViewModel extends AndroidViewModel {
+
+    public SearchViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
