b249874fe1815cb8fdab0e83907baaebf907b7c1
HaYeong <hy78733@gmail.com>
2021-10-04 13:17:24 +0900

    Add: finished potion card ver.2

--- MyPotion/app/src/main/java/com/palebluedot/mypotion/ui/storage/StorageViewModel.java
+++ MyPotion/app/src/main/java/com/palebluedot/mypotion/ui/storage/StorageViewModel.java
@@ -11,7 +11,10 @@ import com.palebluedot.mypotion.data.model.Like;
 import com.palebluedot.mypotion.data.model.MyPotion;
 import com.palebluedot.mypotion.data.repository.like.LikeRepository;
 import com.palebluedot.mypotion.data.repository.mypotion.MyPotionRepository;
+import com.palebluedot.mypotion.util.MyUtil;
+import com.palebluedot.mypotion.util.TagManager;
 
+import java.util.Date;
 import java.util.List;
 
 public class StorageViewModel extends AndroidViewModel {
@@ -19,6 +22,7 @@ public class StorageViewModel extends AndroidViewModel {
     private LikeRepository likeRepository;
     public MutableLiveData<List<MyPotion>> mPotionList;
     public MutableLiveData<List<Like>> mLikeList;
+    public MutableLiveData<MyPotion> mPotion;
 
     public StorageViewModel(Application application) {
         super(application);
@@ -30,5 +34,22 @@ public class StorageViewModel extends AndroidViewModel {
         this.mLikeList.postValue(likeRepository.getLikeList());
     }
 
+    public String getEffect() {
+        if(mPotion.getValue() != null)
+            return TagManager.getInstance().listToString(mPotion.getValue().effectTags);
+        else
+            return null;
+    }
+    public String getIngDays() {
+        if(mPotion.getValue() == null){
+            return null;
+        }
 
+        String beginStr = mPotion.getValue().beginDate;
+        Date today = MyUtil.getFormattedToday();
+        Date beginDate = MyUtil.stringToDate(beginStr);
+        long msDiff = today.getTime() - beginDate.getTime();
+        long dayDiff = Math.abs(msDiff) / (24 * 60 * 60 * 1000);
+        return "총 "+ dayDiff +"일";
+    }
 }
